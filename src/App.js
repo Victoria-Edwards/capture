aa0a4e98e6798afc05675385c84544f585786234
Victoria-Edwards <victoriaedwards3773@gmail.com>
2021-01-09 18:43:07 +0000

    5. Intro to styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//Global styles
+import GlobalStyle from "./components/GlobalStyled";
 //Import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
